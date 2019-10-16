bfe25907b6048c0149b2dac6030314e0a7a6178d
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-16 16:04:24 -0700

    sever up

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,55 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
+    useNullasDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: "./database/authorized.db3"
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     },
     pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      afterCreate: (conn, done) => {
+        // enforces foreign key constraints on SQLite, not needed for other DBMS
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     }
   }
 
+  //   staging: {
+  //     client: "postgresql",
+  //     connection: {
+  //       database: "my_db",
+  //       user: "username",
+  //       password: "password"
+  //     },
+  //     pool: {
+  //       min: 2,
+  //       max: 10
+  //     },
+  //     migrations: {
+  //       tableName: "knex_migrations"
+  //     }
+  //   },
+
+  //   production: {
+  //     client: "postgresql",
+  //     connection: {
+  //       database: "my_db",
+  //       user: "username",
+  //       password: "password"
+  //     },
+  //     pool: {
+  //       min: 2,
+  //       max: 10
+  //     },
+  //     migrations: {
+  //       tableName: "knex_migrations"
+  //     }
+  //   }
 };
