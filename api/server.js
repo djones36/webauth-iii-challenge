4a8d1af97ce043c53056cddfbbe9bfad597b57e1
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-16 21:47:19 -0700

    created registration route

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const logger = require("../middleware/loggerMiddleware");
 
 //Routes imports
-// const tokenRegisterRoute = require('./routes/tokenRegisterRoute');
+const tokenRegisterRoute = require("./routes/tokenRegisterRoute");
 // const tokenLoginRoute = require('./routes/tokenLoginRoute')
 // const AuthUserRoute = require('./routes/authorization/authUserRoute');
 // const tokenLogoutRoute = require('./routes/tokenLogoutRoute');
@@ -15,7 +15,7 @@ const logger = require("../middleware/loggerMiddleware");
 server.use(helmet(), express.json(), logger, cors());
 
 //Routes
-// server.use("/api/register", tokenRegisterRoute);
+server.use("/api/register", tokenRegisterRoute);
 // server.use("/api/login", tokenLoginRoute);
 // server.use("/api/authorization/users", AuthUserRoute);
 // server.use("/api/logout", tokenLogoutRoute);
