5e32e4aea4906d9538f58a68d9a1dc9e9c762f31
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-16 20:06:03 -0700

    laying out routes

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
+
+//Routes imports
+// const tokenSignupRoute = require('./routes/tokenSignUpRoute');
+// const tokenLoginRoute = require('./routes/tokenLoginRoute')
+// const AuthUserRoute = require('./routes/authorization/authUserRoute');
+// const tokenLogoutRoute = require('./routes/tokenLogoutRoute');
+
+server.use(helmet(), express.json(), mw.logger, cors());
+
+//Routes
+// server.use("/api/signup", tokenSignupRoute);
+// server.use("/api/login", tokenLoginRoute);
+// server.use("/api/authorization/users", AuthUserRoute);
+// server.use("/api/logout", tokenLogoutRoute);
+
+//Deployment
+server.get("/", (req, res) => {
+  res.status(200).json("Successful Deployment");
+});
+
+module.exports = server;
