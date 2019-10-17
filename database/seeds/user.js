0645589bd0ba2650c03e464357f7909798b6666c
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-16 21:03:24 -0700

    seeds working

--- database/seeds/user.js
+++ database/seeds/user.js
@@ -1,11 +1,24 @@
+const bcrypt = require("bcryptjs");
+
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
   return knex("users").then(function() {
     // Inserts seed entries
     return knex("users").insert([
-      { username: 1, passsword: "password1", departments: "departments1" },
-      { username: 2, passsword: "password2", departments: "departments2" },
-      { username: 3, passsword: "password3", departments: "departments3" }
+      {
+        username: "user1",
+        password: "password1",
+        departments: "departments1"
+      },
+      {
+        username: "user2",
+        password: "password2",
+        departments: "departments2"
+      },
+      {
+        username: "user3",
+        password: "password3",
+        departments: "departments3"
+      }
     ]);
   });
 };
